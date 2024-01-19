17838265969e554a1e7d682f8f14233f970fb0b1
Minh Y <nguyenhominhy@gmail.com>
2024-01-19 13:14:01 +0700

    them laykhach

--- ProjectCNDN_Revamp/PROJECTCNDN/DataAccessLayer/GetData.cs
+++ ProjectCNDN_Revamp/PROJECTCNDN/DataAccessLayer/GetData.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace DataAccessLayer
 {
     public class GetData
     {
-        
+        public static DataTable LayKhachHang()
+        {
+            using(DataAccess db =new DataAccess())
+            {
+                var customers = db.DSCustomer.ToList();
+                DataTable dt = new DataTable();
+                foreach(var prop in typeof(DataTable).GetProperties())
+                {
+                    dt.Columns.Add(prop.Name, prop.PropertyType);
+                }
+                foreach(var customer in customers)
+                {
+                    DataRow dr = dt.NewRow();
+                    foreach(var prop in typeof (DataTable).GetProperties())
+                    {
+                        dr[prop.Name] = prop.GetValue(customer);
+                    }
+                    dt.Rows.Add(dr);
+                }
+                return dt;
+            }
+        }
     }
 }
